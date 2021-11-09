ef1050c343661344118cafa501c4cbb3343f7827
OwenWong <owen.cq.cn@gmail.com>
2021-11-09 16:15:42 +0800

    chore: update webpack cfg

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,6 @@
  * @Date: 2021-06-08 15:18:45
  */
 const configs = require('@otools/compile-tools')
-const PROD = process.env.NODE_ENV === 'production'
-const config = PROD ? configs.prod : configs.dev
+configs.module.rules.find(({ test }) => test.test('.ts')).use.pop()
 
-config.module.rules.find(({ test }) => test.test('.ts')).use.pop()
-
-module.exports = config
+module.exports = configs
