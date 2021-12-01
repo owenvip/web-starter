80c878f0504b0185c063ad30d45f63f11d5fd1ba
owenwong <owen.cq.cn@gmail.com>
2021-12-01 20:11:26 +0800

    chore: update deps

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+/*
+ * @Descripttion:
+ * @Author: OwenWong
+ * @Email: owen.cq.cn@gmail.com
+ * @Date: 2021-06-03 21:41:28
+ */
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+  },
+  extends: '@otools/eslint-config-otool',
+}
